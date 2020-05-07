26c5a030870e148c65104b06a5faa20a594339e0
pisonmison <58531355+pisonmison@users.noreply.github.com>
2020-05-07 15:01:37 +0200

    Update: 2 Acitivies, man kann mit button von 1 zu 2 switchen.

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.test;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.testbutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                testausgabe();
+            }
+        });
+    }
+
+
+    public void testausgabe() {
+            Intent intent = new Intent(this, Activity2.class);
+            startActivity(intent);
     }
 }
